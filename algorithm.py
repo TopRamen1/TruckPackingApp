7f04e3595c93246ec3cf2ccbab7f5d253125ad22
baradejoo <baradziejkamil@gmail.com>
2022-12-05 22:14:45 +0100

    added function to print solution and object function

--- algorithm.py
+++ algorithm.py
@@ -79,3 +79,59 @@ def genetic_alg(data: MainStorage, it_num: int, pop_size: int, cross: float, mut
         plt.show()
 
     return best_sol, best_sol_vec, av_sol_vec
+
+
+def print_sol(data: MainStorage, best_sol):
+    """Prints best solution"""
+
+    p_to_t = {}
+
+    for i in range(0, len(best_sol.ch_p)):
+        if best_sol.ch_p[i] in p_to_t.keys():
+            p_to_t[best_sol.ch_p[i]].append(i)
+        else:
+            p_to_t[best_sol.ch_p[i]] = [i]
+
+    print("Koszt: ", best_sol.obj_fcn)
+
+    for k, v in p_to_t.items():
+        sum = 0
+        for e in v:
+            sum += data.list_of_packages[e].weight
+
+        print(k, "ladowność: ", data.list_of_trucks[k].load, ", paczki: ", v, " suma wag: ", sum, " adres: ",
+              data.list_of_packages[v[0]].address)
+
+    return p_to_t
+
+
+def obj_fcn(data_mst: MainStorage, data_ind: Individual) -> float:
+    """Calculate function which describes our problem
+       :return: value of above function"""
+
+    act_truck_pos = [i for i, m in enumerate(data_ind.ch_t) if m != -1]  # truck's id who has defined storage
+    act_package_pos = [j for j, p in enumerate(data_ind.ch_p)]  # package's id
+    sum_1, sum_2, sum_3 = 0, 0, 0
+
+    # Sum_1:
+    for i in act_truck_pos:
+        sum_1 += data_mst.list_of_trucks[i].exp_cost
+
+    # Sum_2:
+    for i in act_truck_pos:
+        sum_2 += data_mst.list_of_storages[data_ind.ch_t[i]].distance / 100 * data_mst.k * \
+                 data_mst.list_of_trucks[i].min_fuel_use
+
+    # Sum_3:
+    for i in act_truck_pos:
+        sum_packages = 0
+        for j in act_package_pos:
+            if i == data_ind.ch_p[j]:
+                sum_packages += data_mst.list_of_packages[j].weight
+        sum_3 += data_mst.list_of_storages[data_ind.ch_t[i]].distance / 100 * data_mst.k * \
+                 (data_mst.list_of_trucks[i].max_fuel_use - data_mst.list_of_trucks[i].min_fuel_use) * \
+                 (sum_packages / data_mst.list_of_trucks[i].load)
+
+    final_result = sum_1 + sum_2 + sum_3
+
+    return final_result
