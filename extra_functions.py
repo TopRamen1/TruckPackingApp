b80fe2903b5cdecbde84ec222f0acd3274a13bf2
baradejoo <baradziejkamil@gmail.com>
2022-12-05 22:05:08 +0100

    added generetae list function

--- extra_functions.py
+++ extra_functions.py
@@ -16,3 +16,23 @@ def flat_list(org_list_: List[List[int]]) -> List[int]:
             flat_lst.append(sublist)
 
     return flat_lst
+
+
+def ge_rand(list_divisors_: List[List[int]], num_cross_points_: List[int]) -> Tuple[List[List[int]], List[List[int]]]:
+    """Generate lists which tells you which genes to take from a particular parent (used in function: crossover)"""
+
+    rand_lst1 = []  # Shows which part of divided chromosome stay from parent 1
+    rand_lst2 = []  # Shows which part of divided chromosome stay from parent 2
+    for i, j in enumerate(list_divisors_):
+        temp = random.sample(range(len(j)), int(num_cross_points_[i] / 2))
+        rand_lst1.append(temp)
+        temp2 = []
+        for p, k in enumerate(j):
+            if p not in temp:
+                temp2.append(p)
+        rand_lst2.append(temp2)
+    del temp, temp2
+
+    tup_of_rand_lst = (rand_lst1, rand_lst2)
+
+    return tup_of_rand_lst
